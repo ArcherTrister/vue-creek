5f25d07d2a04a3c9020982f14eb3f49885e1aa00
廖雄 <ArcherTrister@outlook.com>
2019-04-28 11:05:55 +0800

    更新包

--- build/bin/version.js
+++ build/bin/version.js
@@ -1,10 +1,13 @@
 var version = process.env.VERSION || require('../../package.json').version;
 var content = {
+  '1.0.0': '1.0',
+  '1.0.1': '1.0',
+  '1.0.2': '1.0',
   '1.0.3': '1.0',
-  '2.0.11': '2.0',
-  '2.1.0': '2.1',
-  '2.2.2': '2.2',
-  '2.3.9': '2.3',
-  '2.4.11': '2.4'
+  '1.0.4': '1.0',
+  '1.0.5': '1.0',
+  '1.0.6': '1.0',
+  '1.0.7': '1.0',
+  '1.0.8': '1.0'
 };
-if (!content[version]) content[version] = '2.5';
+if (!content[version]) content[version] = '1.0';
